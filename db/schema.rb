f1067ef4b3bb9ef56f7a38f331a86ab1754a02d4
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-06-01 18:33:29 +0100

    Add Account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_142153) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_01_165349) do
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.integer "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_accounts_on_owner_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_agent"
@@ -29,5 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_01_142153) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "accounts", "users", column: "owner_id"
   add_foreign_key "sessions", "users"
 end
