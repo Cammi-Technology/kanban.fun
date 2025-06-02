fdbb6d4655baf9a839ee7023e290d1140bd7d582
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-06-02 08:05:44 +0100

    Add minitest-reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new(print_failure_summary: true,suppress_inline_failure_output: true)
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
