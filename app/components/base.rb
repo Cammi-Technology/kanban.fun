4d7ad73fce2987a9bbfbcab65cab314ad06c9667
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-06-28 15:37:38 +0100

    Accept all kwargs in base component
    By acepting all kwargs this allows us to easily match the html "api", as
    well as allowing as to merge them with defaults that we set in
    `default_attrs`

--- app/components/base.rb
+++ app/components/base.rb
@@ -3,12 +3,24 @@
 class Components::Base < Phlex::HTML
   extend Literal::Properties
 
+  include Phlex::Rails::Helpers::T
   include Phlex::Rails::Helpers::Routes
+
+  prop :attrs, Hash, :**, reader: :private do |value|
+    merge_attrs(value, default_attrs)
+  end
+
+  def merge_attrs(user_attrs, default_attrs)
+    mix(default_attrs, user_attrs)
+  end
+
+  def default_attrs = {}
 
   if Rails.env.development?
     def before_template
       comment { "Before #{self.class.name}" }
       super
+      comment { "After #{self.class.name}" }
     end
   end
 end
--- app/views/base.rb
+++ app/views/base.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class Views::Base < Components::Base
-  include Phlex::Rails::Helpers::T
-  include Phlex::Rails::Helpers::Routes
-
   def around_template
     render Components::ApplicationLayout.new(page_info) do
       super
