f9c1c82a55d0a69db8fc62b6fde34260a550d43d
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-06-28 17:26:44 +0100

    Remove other system tests

--- test/system/accounts_test.rb
+++ test/system/accounts_test.rb
@@ -5,11 +5,6 @@ class AccountsTest < ApplicationSystemTestCase
     @account = accounts(:account)
   end
 
-  test "visiting the index" do
-    visit accounts_url
-    assert_selector "h1", text: "Accounts"
-  end
-
   test "should create account" do
     sign_in_as(users(:user_without_account))
 
@@ -18,22 +13,5 @@ class AccountsTest < ApplicationSystemTestCase
     click_on "Create Account"
 
     assert_text t("accounts.create.success")
-  end
-
-  test "should update Account" do
-    visit account_url(@account)
-    click_on "Edit this account", match: :first
-
-    fill_in "Name", with: "Kanemitsu Corporation"
-    click_on "Update Account"
-
-    assert_text "Account was successfully updated"
-  end
-
-  test "should destroy Account" do
-    visit account_url(@account)
-    click_on "Destroy this account", match: :first
-
-    assert_text "Account was successfully destroyed"
   end
 end
