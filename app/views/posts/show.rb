ff422455be9c439c70faf356b8e73018bc4ade38
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-08-13 17:52:14 +0100

    Display comments and update tests

--- app/views/posts/show.rb
+++ app/views/posts/show.rb
@@ -6,6 +6,18 @@ class Views::Posts::Show < Views::Base
   def view_template
     h1 { post.title }
     raw safe(post.content.to_s)
+
+    div do
+      h2 { "Comments" }
+
+      post.comments.each do |comment|
+        div do
+          raw safe(comment.content.to_s)
+        end
+      end
+
+      CommentForm(record: post)
+    end
   end
 
   def page_title = post.title
--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -36,4 +36,33 @@ class PostsTest < ApplicationSystemTestCase
     visit account_project_posts_url(@account, @project)
     assert_text "Nuke Initiative for increasing crime in Detroit City"
   end
+
+  test "should display comment form on post show page" do
+    sign_in_as(@account_owner)
+    assert_current_path accounts_path, wait: true
+
+    post = posts(:post)
+    visit account_project_post_path(@account, @project, post)
+
+    assert_text "Comments"
+    assert_selector "form[action*='comments']"
+    assert_selector "trix-editor"
+    assert_button "Add Comment"
+  end
+
+  test "should create comment on post" do
+    sign_in_as(@account_owner)
+    assert_current_path accounts_path, wait: true
+
+    post = posts(:post)
+    visit account_project_post_path(@account, @project, post)
+
+    fill_in_rich_text_area "comment_content", with: "This is a test comment"
+    click_on "Add Comment"
+
+    # Verify success notice appears
+    assert_text t("comments.create.success"), wait: true
+    # Now verify comment is displayed
+    assert_text "This is a test comment"
+  end
 end
